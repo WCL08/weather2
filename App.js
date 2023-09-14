0e3bab979771d7965f5c3c14db637f8e758e2d3e
PC\PC <tokee678@daum.net>
2023-09-14 21:54:17 +0900

    [Feat] 환경변수 관리

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator
+} from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
+import { API_KEY } from "@env"
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window")
-const API_KEY = "09199781d3f85f58f05240c97b108d2c"
 
 export default function App() {
   const [city, setCity] = useState("Loding...")
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  // 위치 API => Location
+  // 위치,날씨 API
   const getWeather = async () => {
+    // 위치 API  
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -18,19 +27,31 @@ export default function App() {
     const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 })
     const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false })
     setCity(location[0].city)
+    // 원래 날씨 API [변경이유 유료]
     // const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
     // const json = await response.json();
     // console.log(json.daliy)
+    // 날씨 API
     const { list } = await (
       await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
     ).json();
     const filteredList = list.filter(({ dt_txt }) => dt_txt.endsWith("00:00:00"));
     setDays(filteredList);
   };
+  const Icons = {
+    Clear: "day-sunny",
+    Clouds: "cloudy",
+    Rain: "rain",
+    Atmosphere: "cloudy-gusts",
+    Snow: "snow",
+    Drizzle: "day-rain",
+    Thunderstorm: "lightning",
+  };
   useEffect(() => {
     getWeather();
   }, [])
 
+  // console.log("API_KEY = ",API_KEY)
   return (
     <View style={styles.container}>
       <View style={styles.city}>
@@ -42,7 +63,7 @@ export default function App() {
         horizontal
         contentContainerStyle={styles.weather}>
         {days.length === 0 ? (
-          <View style={styles.day}>
+          <View style={{...styles.day, alignItems: "center"}}>
             <ActivityIndicator
               style={{ marginTop: 10 }}
               color="white"
@@ -51,8 +72,14 @@ export default function App() {
           </View>
         ) : (
           days.map((day, index) => (
-            <View key={index} style={styles.day}>
-              <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+            <View key={index} style={{...styles.day,padding: 20 }}>
+              <View style={styles.icon}>
+                <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+                <Fontisto
+                  name={Icons[day.weather[0].main]}
+                  size={68}
+                  color="white" />
+              </View>
               <Text style={styles.description}>{day.weather[0].main}</Text>
               <Text style={styles.tinyText}>{day.weather[0].description}</Text>
             </View>
@@ -82,17 +109,25 @@ const styles = StyleSheet.create({
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 100,
+    color: "white",
   },
   description: {
-    marginTop: -30,
-    fontSize: 50
+    marginTop: -10,
+    fontSize: 30,
+    color: "white",
   },
   tinyText: {
-    fontSize: 20
+    fontSize: 20,
+    color: "white",
+  },
+  icon: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%"
   }
 })
